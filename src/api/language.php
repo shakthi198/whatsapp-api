d54d0506ebc940f5b453ccdd2c2b24a507be169a
Hariprakash02 <117583324+Hariprakash02@users.noreply.github.com>
2025-10-27 17:35:33 +0530

    changes

--- src/api/language.php
+++ src/api/language.php
@@ -4,7 +4,6 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Allow-Headers: Content-Type");
 
-require_once "config.php";
 
 require_once "config.php";
 
